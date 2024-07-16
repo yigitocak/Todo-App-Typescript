9bf57622034b7d4e4cfdf965b91d4825ae1ad8c8
yigit ocak <yigitockk@gmail.com>
2024-07-16 17:31:02 -0400

    knexfile init

---
+++ server/knexfile.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+
+const knex = {
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    charset: "utf8",
+  },
+};
+
+export default knex;
