688568dcefb3e38dcf822b8ba431381e8f938be9
yigit ocak <yigitockk@gmail.com>
2024-07-16 22:19:21 -0400

    fix the tables logic

--- server/migrations/20240716204813_create_user_todo_tables.js
+++ server/migrations/20240716204813_create_user_todo_tables.js
@@ -3,31 +3,12 @@
  * @returns { Promise<void> }
  */
 export async function up(knex) {
-  return knex.schema
-    .createTable("user", (table) => {
-      table.increments("id");
-      table.string("name").notNullable();
-      table.timestamp("created_at").defaultTo(knex.fn.now());
-      table
-        .timestamp("updated_at")
-        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
-    })
-    .createTable("todos", (table) => {
-      table.increments("id");
-      table.integer("user_id").unsigned().notNullable();
-      table.string("todo", 100).notNullable();
-      table.boolean("completed").notNullable();
-      table
-        .foreign("user_id")
-        .references("id")
-        .inTable("user")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE");
-      table.timestamp("created_at").defaultTo(knex.fn.now());
-      table
-        .timestamp("updated_at")
-        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
-    });
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("email").notNullable().unique();
+    table.string("password", 255).notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 }
 
 /**
@@ -35,5 +16,5 @@ export async function up(knex) {
  * @returns { Promise<void> }
  */
 export async function down(knex) {
-  return knex.schema.dropTable("todos").dropTable("user");
+  return knex.schema.dropTable("todos").dropTable("users");
 }
