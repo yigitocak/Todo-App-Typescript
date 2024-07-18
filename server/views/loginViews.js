e61f1f718e369a34ece4f314afa00ca3700e48a2
yigit ocak <yigitockk@gmail.com>
2024-07-18 01:44:17 -0400

    add login views

---
+++ server/views/loginViews.js
@@ -0,0 +1,61 @@
+import knex from "knex";
+import knexfile from "../knexfile.js";
+import bcrypt from "bcrypt";
+const db = knex(knexfile.development);
+import jtw from "jsonwebtoken";
+import "dotenv/config";
+
+export const loginUserView = async (req, res) => {
+  const { email, password, rememberMe } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Bad request body.",
+    });
+  }
+
+  try {
+    const user = await db("users").where({ email }).first();
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    if (password && user.password) {
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return res.status(401).json({
+          message: "Incorrect Password",
+          success: false,
+        });
+      }
+    }
+
+    const SECRET_KEY = process.env.SECRET_KEY;
+
+    const expiresIn = rememberMe ? "7d" : "12h";
+    const token = jtw.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      SECRET_KEY,
+      { expiresIn },
+    );
+
+    return res.status(200).json({
+      success: true,
+      token,
+      message: rememberMe ? "Logged in for 7d" : "Logged in for 12h",
+      email,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
