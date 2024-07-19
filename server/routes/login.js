470aa267f055435652dc7178a2f8a1305c7eb2f5
yigit ocak <yigitockk@gmail.com>
2024-07-19 18:39:07 -0400

    add route for authenticate user

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,10 +1,13 @@
 import express from "express";
 const login = express.Router();
 
-import { loginUserView } from "../views/loginViews.js";
+import { loginUserView, decodeView } from "../views/loginViews.js";
+import authenticateToken from "../middlewares/authenticateToken.js";
 
 login.use(express.json());
 
 login.post("/", loginUserView);
+
+login.get("/auth", authenticateToken, decodeView);
 
 export default login;
