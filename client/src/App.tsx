2ea3e382bdd976aba4a4586117dce2a70712d072
yigit ocak <yigitockk@gmail.com>
2024-07-19 18:58:13 -0400

    add snackbar and user authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,56 @@
-import { BackgroundHeading } from "./Components/BackgroundHeading.tsx";
-import { Footer } from "./Components/Footer.tsx";
-import { TodoList } from "./Components/TodoList.tsx";
-import { Header } from "./Components/Header.tsx";
-import { Sidebar } from "./Components/Sidebar.tsx";
+import { BackgroundHeading } from "./Components/BackgroundHeading";
+import { Footer } from "./Components/Footer";
+import { TodoList } from "./Components/TodoList";
+import { Header } from "./Components/Header";
+import { Sidebar } from "./Components/Sidebar";
+import { useEffect } from "react";
+import { Snackbar } from "./Components/Snackbar";
+import { useAppContext } from "./contexts/AppContextProvider";
+import axios from "axios";
+import { AUTH_ENDPOINT, BASE_URL } from "./utils/utils.ts";
+import Cookies from "js-cookie";
 
 const App = () => {
+  const {
+    loggedIn,
+    setLoggedIn,
+    getTodos,
+    snackbarMessage,
+    snackbarOpen,
+    setSnackbarOpen,
+    setSnackbarMessage,
+    setUserEmail,
+  } = useAppContext();
+
+  const token = Cookies.get("authToken");
+
+  const verifyToken = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}${AUTH_ENDPOINT}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (response.data.success === true) {
+        setUserEmail(response.data.decoded);
+        setLoggedIn(true);
+      }
+    } catch (e) {
+      setSnackbarMessage("Error when authenticating token");
+      setSnackbarOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      verifyToken();
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (loggedIn) {
+      getTodos();
+    }
+  }, [loggedIn]);
+
   return (
     <div className="font-sans bg-[#6199d4] h-screen flex flex-col justify-center items-center">
       <BackgroundHeading />
@@ -16,6 +62,11 @@ const App = () => {
         </section>
       </main>
       <Footer />
+      <Snackbar
+        message={snackbarMessage}
+        open={snackbarOpen}
+        onClose={() => setSnackbarOpen(false)}
+      />
     </div>
   );
 };
