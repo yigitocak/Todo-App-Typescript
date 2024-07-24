22a69bf47b07bb924a399e0ee216c4deb859b1f0
yigit ocak <yigitockk@gmail.com>
2024-07-24 12:54:04 -0400

    delete redundant code

--- client/src/Components/TodoList.tsx
+++ client/src/Components/TodoList.tsx
@@ -8,20 +8,28 @@ type TodoItemType = {
 };
 
 export const TodoList = () => {
-  const { todoList } = useAppContext();
+  const { todoList, loggedIn } = useAppContext();
 
   const sortedTodoList = todoList.sort((a, b) => b.timestamp - a.timestamp);
 
   return (
-    <ul className="max-sm:flex-1 flex-1">
-      {sortedTodoList.map((item: TodoItemType) => (
-        <TodoItem
-          todo={item.todo}
-          id={item.id}
-          completed={item.completed}
-          key={item.id}
-        />
-      ))}
-    </ul>
+    <>
+      {loggedIn ? (
+        <ul className="max-sm:flex-1 flex-1">
+          {sortedTodoList.map((item: TodoItemType) => (
+            <TodoItem
+              todo={item.todo}
+              id={item.id}
+              completed={item.completed}
+              key={item.id}
+            />
+          ))}
+        </ul>
+      ) : (
+        <div className="max-sm:flex-1 flex-1">
+          <p className="font-bold text-center mt-10">Login to add todos!</p>
+        </div>
+      )}
+    </>
   );
 };
