a55f1a7d8eaca39ea10eea4f3eaba58bbc8c62ee
yigit ocak <yigitockk@gmail.com>
2024-07-26 22:51:58 -0400

    vercel config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist", // Ensure the output directory is set correctly
+    },
+    define: {
+      "process.env": {
+        VITE_ENV: env.VITE_ENV,
+        DB_HOST: env.DB_HOST,
+        DB_PORT: env.DB_PORT,
+        DB_NAME: env.DB_NAME,
+        DB_USER: env.DB_USER,
+        DB_PASSWORD: env.DB_PASSWORD,
+      },
+    },
+  };
 });
